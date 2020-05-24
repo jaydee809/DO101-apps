c463505e766d2b457c88c86cd5ecaf535d78f49a
jaydee809 <jd809@hotmail.com>
2020-05-24 15:53:52 +0100

    add /mars endpoint to app

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello J Git World!\n');
+});
+
+app.get('/mars', function (req, res) {
+  res.send('Hello J Git Mars!\n');
 });
 
 app.listen(8080, function () {
